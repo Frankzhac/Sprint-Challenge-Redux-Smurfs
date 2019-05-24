584181622700e0cc0fdd68df7ed059e1c8a4cc4c
Frankzhac <Frank.hemle@gmail.com>
2019-05-24 13:44:08 -0400

    update actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,11 +3,17 @@ import axios from 'axios';
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
-export const START_REQUEST = 'START_REQUEST';
-export const REQUEST_SUCCESS = 'REQUEST_SUCCESS';
-export const REQUEST_FAIL = 'REQUEST_FAIL';
+export const FETCH_SMURF = 'FETCH_SMURF';
+export const ERROR = 'ERROR';
+export const FETCHING_SMURF = 'FETCHING_SMURF';
 export const ADD_SMURF = 'ADD_SMURF';
+export const ADDING_SMURF = 'ADDING_SMURF';
 export const GET_SMURF = 'GET_SMURF';
+export const GETTING_SMURF = 'GETTING_SMURF';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
+export const DELETE_SMURF = 'UPDATE_SMURF';
+export const UPDATING_SMURF = 'UPDATING_SMURF';
+export const DELETING_SMURF = 'UPDATING_SMURF';
 /*
   For this project you'll need at least 2 action creators for the main portion,
    and 2 more for the stretch problem.
@@ -21,31 +27,32 @@ export const GET_SMURF = 'GET_SMURF';
 
 const URL = 'http://localhost:3333/smurfs';
 
-export const startRequest = () => {
-  dispatch({ type: START_REQUEST});
+export const fetchSmurf = id => {
+  dispatch({ type: FETCHING_SMURF});
   return axios
-    .post(`${URl}/request`)
+    .post(`${URl}/fetch`)
     .then(res => {
       console.log(res.data);
       dispatch({
-        type: REQUEST_SUCCESS,
+        type: FETCH_SMURF,
         payload: res.data
       });
       dispatch({
-        type: REQUEST_FAIL,
-        payload: err
+        type: FETCH_SMURF,
+        payload: data
       });
     });
     .catch(err => {
       console.log(err);
+      dispatch({ type: ERROR, payload: err });
     });
  };
 };
 
-export const addSmurf = () => {
+export const addSmurf = smurf => {
   const newSmurf = axios.get(`${URL}/add`, smurf);
   return dispatch => {
-    dispatch({ type: ADD_SMURF });
+    dispatch({ type: ADDING_SMURF });
     newSmurf
       .then(res => {
         dispatch({
@@ -64,7 +71,7 @@ export const addSmurf = () => {
   export const getSmurf = () => {
     const smurf = axios.get(`${URL}/get`);
     return dispatch => {
-      dispatch({ type: GET_SMURF });
+      dispatch({ type: GETTING_SMURF });
       smurf
         .then(res => {
           type: GET_SMURF,
@@ -78,3 +85,19 @@ export const addSmurf = () => {
         })
     }
   }
+
+  export const deleteSmurf = id => {
+  const deletedFriend = axios.delete(`${URL}/delete`, {
+    data: { id }
+  });
+  return dispatch => {
+    dispatch({ type: DELETING_SMURF });
+    deletedFriend
+      .then(({ data }) => {
+        dispatch({ type: DELETE_SMURF, payload: data });
+      })
+      .catch(err => {
+        dispatch({ type: ERROR, payload: err });
+      });
+  };
+};
